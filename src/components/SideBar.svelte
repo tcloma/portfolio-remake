e333dc2369329081ad3e37dcc894820cdb91c446
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2023-02-05 16:50:36 -0500

    setup darkmode toggling

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -1,16 +1,43 @@
 <script lang="ts">
 	import NavBtn from './NavBtn.svelte';
+	import LightBulbOff from './icons/LightBulbOff.svelte';
+	import LightBulbOn from './icons/LightBulbOn.svelte';
+	import IconWrapper from './icons/IconWrapper.svelte';
+
 	export let pageUrl: string;
+	$: currPage = pageUrl.slice(1);
 
 	const btns = ['home', 'work', 'blog'];
-	$: currPage = pageUrl.slice(1);
+	$: isDarkMode = true;
+
+	function toggleMode() {
+		isDarkMode = !isDarkMode;
+		if (isDarkMode) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}
 </script>
 
 <aside class="fixed ml-[30%] mt-20 text-center">
-	<span class="text-4xl font-bold text-secondary">Ty</span>
+	<IconWrapper
+		on:click={toggleMode}
+		on:keydown={toggleMode}
+	>
+		{#if isDarkMode}
+			<LightBulbOff />
+		{:else}
+			<LightBulbOn />
+		{/if}
+	</IconWrapper>
+
 	<nav class="flex flex-col">
 		{#each btns as btnName}
-			<NavBtn name={btnName} page={currPage}/>
+			<NavBtn
+				name={btnName}
+				page={currPage}
+			/>
 		{/each}
 	</nav>
 </aside>
---
+++ src/components/icons/IconWrapper.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div
+	on:click
+	on:keydown
+	role="button"
+	tabindex="0"
+	class="p-2"
+>
+	<slot />
+</div>
